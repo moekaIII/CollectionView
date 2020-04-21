e90db79cfab85f08b49831730a64dbf038d88444
ihara moeka <ihara.moeka@gmail.com>
2020-04-21 11:45:09 +0900

    delete caches

---
+++ CollectionView/Top/TopViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TopViewController.swift
+//  CollectionView
+//
+//  Created by 伊原萌夏 on 2020/04/15.
+//  Copyright © 2020 Playground, Inc. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+}
+
+// MARK: - method
+extension TopViewController {
+}
