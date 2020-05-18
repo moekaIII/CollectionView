13db5b332db25b04095aed95b91e7f8f2c0c232e
ihara moeka <ihara.moeka@gmail.com>
2020-05-18 13:46:26 +0900

    add TestViewController

---
+++ CollectionView/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  CollectionView
+//
+//  Created by 伊原萌夏 on 2020/05/18.
+//  Copyright © 2020 Playground, Inc. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
